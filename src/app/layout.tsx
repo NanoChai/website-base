3af17b0a36e1caed489d9be43ab716d97df45dad
sonicsmith <thesonicsmith@gmail.com>
2024-11-15 22:12:55 +0700

    Connect NavBar with Dynamic login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { WalletProvider } from "@/provider/WalletProvider";
+import { NavBar } from "@/components/NavBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <WalletProvider>{children}</WalletProvider>
+        <WalletProvider>
+          <NavBar />
+          {children}
+        </WalletProvider>
       </body>
     </html>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,5 +3,9 @@
 import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
 
 export const NavBar = () => {
-  return <div className="flex justify-end">{/* <DynamicWidget /> */}</div>;
+  return (
+    <div className="flex justify-end p-3 bg-gray-300">
+      <DynamicWidget />
+    </div>
+  );
 };
