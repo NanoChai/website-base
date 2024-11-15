a033535c9b39cd50afdf0f01b6d1801fb3d9923b
sonicsmith <thesonicsmith@gmail.com>
2024-11-15 22:23:40 +0700

    Remove template page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
 
 export const NavBar = () => {
   return (
-    <div className="flex justify-end p-3 bg-gray-300">
+    <div className="flex justify-end p-3 bg-gray-300 sticky top-0 z-60">
       <DynamicWidget />
     </div>
   );
