fbfe915b60be33ad1c5c3f2c9bb019eb981f8048
sonicsmith <thesonicsmith@gmail.com>
2024-11-16 19:24:26 +0700

    Set chainId to base sepolia

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ export const USER_PAYMENT_HEADER = "user-payment-signature";
 export const RESTAKER_PAYMENT_HEADER = "restaker-payment-signature";
 export const SERVICE = "0xefDD4C11efD4df6F1173150e89102D343ae50AA4"; // Providers Wallet
 export const AMOUNT = BigInt(1);
-export const CHAIN_ID = BigInt(1);
+export const CHAIN_ID = BigInt(84532);
 
 export const abi = parseAbi([
   "function deposit(uint256 amount) external",
